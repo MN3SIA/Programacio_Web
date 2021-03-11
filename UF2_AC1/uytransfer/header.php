ae9bf9e5382828e183ab90c9c0aa4de57467232b
MN3SIA <alex.martin@mataro.epiaedu.cat>
2021-03-11 16:45:26 +0100

    Afegit el header

---
+++ UF2_AC1/uytransfer/header.php
@@ -0,0 +1,8 @@
+<header class="navbar row bg-secondary">
+    <h1 class="col-md col-sm-12 pl-5">Uy!Transfer</h1>
+    <div class="col-md col-sm-12 text-center">
+        <a class="col-sm border-right border-dark">Enviar archivo</a>
+        <a class="col-sm">Mis últimos archivos</a>
+    </div>
+</header>
+
